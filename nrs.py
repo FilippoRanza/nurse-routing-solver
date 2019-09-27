581ac0cfd9358355bca3f3fa9117fc4b39cf9d15
Filippo <filipporanza@gmail.com>
2019-09-27 17:08:08 +0200

    First Commit

---
+++ nrs.py
@@ -0,0 +1,18 @@
+#! /usr/bin/python3
+
+from argparse import ArgumentParser
+
+
+def parse_args():
+    parser = ArgumentParser()
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+
+
+if __name__ == '__main__':
+    main()
+
+
---
+++ nrs_lib/__init__.py
@@ -0,0 +1,3 @@
+#! /usr/bin/python3
+
+# Copyright (c) 2019 Filippo Ranza <filipporanza@gmail.com>

740e0f83b6eb1bce9cc6a341cf52c08ab9bcac2b
Filippo Ranza <filipporanza@gmail.com>
2019-09-28 16:51:25 +0200

    add some basic cli args

--- nrs.py
+++ nrs.py
@@ -1,18 +1,27 @@
 #! /usr/bin/python3
 
 from argparse import ArgumentParser
-
+import json
 
 def parse_args():
     parser = ArgumentParser()
+    parser.add_argument('instance', help='Request file')
+    parser.add_argument('-o', '--output', 
+                        help='Specify output file')
+    
+
     return parser.parse_args()
 
 
+def load_instance(file_name):
+    with open(file_name) as data:
+        out = json.load(data)
+    return out
+
 def main():
     args = parse_args()
-
+    instance = load_instance(args.instance)
+    print(instance)
 
 if __name__ == '__main__':
     main()
-
-

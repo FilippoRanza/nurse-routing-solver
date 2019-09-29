0fd109290aa8ec81c79dea04229a8ba86da91cfc
Filippo Ranza <filipporanza@gmail.com>
2019-09-29 19:03:09 +0200

    automatic reformatted

--- nrs_lib/model.py
+++ nrs_lib/model.py
@@ -6,9 +6,10 @@
 try:
     from gurobipy import *
 except ImportError:
-    print('this error is silenced for testing purpose')
+    print("this error is silenced for testing purpose")
 from .build_distance import build_distance
 from .subtour_constraint import build_tours
+
 
 def arange(size):
     return list(range(size))
@@ -22,35 +23,51 @@ class ModelConfigutator:
         self.nurses = arange(nurse_count)
         self.nodes = arange(patient_count)
 
-        transit_keys = ((n, i, j) for n in self.nurses for i in self.nodes for j in self.nodes)
-        self.transit_vars = self.model.addVars(transit_keys, name="transit", vtype=GRB.BINARY)
+        transit_keys = (
+            (n, i, j) for n in self.nurses for i in self.nodes for j in self.nodes
+        )
+        self.transit_vars = self.model.addVars(
+            transit_keys, name="transit", vtype=GRB.BINARY
+        )
 
         service_key = ((i, n) for i in self.nodes[1:] for n in self.nurses)
-        self.service_vars = self.model.addVars(service_key, name="service", vtype=GRB.BINARY)
+        self.service_vars = self.model.addVars(
+            service_key, name="service", vtype=GRB.BINARY
+        )
 
-        self.patient_vars = self.model.addVars(self.nodes[1:], name="patient", vtype=GRB.BINARY)
+        self.patient_vars = self.model.addVars(
+            self.nodes[1:], name="patient", vtype=GRB.BINARY
+        )
 
         self._apply_contrains_()
 
     def _apply_contrains_(self):
         for i in self.nodes[1:]:
-            self.model.addConstr(self.service_vars.sum(i, "*") == (1 - self.patient_vars[i]))
+            self.model.addConstr(
+                self.service_vars.sum(i, "*") == (1 - self.patient_vars[i])
+            )
 
         for i in self.nodes[1:]:
             for n in self.nurses:
-                self.model.addConstr(self.transit_vars.sum(n, i, "*") == self.service_vars.sum(i, n))
-                self.model.addConstr(self.transit_vars.sum(n, "*", i) == self.service_vars.sum(i, n))
+                self.model.addConstr(
+                    self.transit_vars.sum(n, i, "*") == self.service_vars.sum(i, n)
+                )
+                self.model.addConstr(
+                    self.transit_vars.sum(n, "*", i) == self.service_vars.sum(i, n)
+                )
 
         for k in self.nurses:
             self.model.addConstr(
                 quicksum(
-                    self.service_vars.sum(i, k) * self.transit_vars.sum(k, 0, i) for i in self.nodes[1:]
+                    self.service_vars.sum(i, k) * self.transit_vars.sum(k, 0, i)
+                    for i in self.nodes[1:]
                 )
                 == 1
             )
             self.model.addConstr(
                 quicksum(
-                    self.service_vars.sum(i, k) * self.transit_vars.sum(k, i, 0) for i in self.nodes[1:]
+                    self.service_vars.sum(i, k) * self.transit_vars.sum(k, i, 0)
+                    for i in self.nodes[1:]
                 )
                 == 1
             )
@@ -65,27 +82,35 @@ class ModelConfigutator:
             for k in self.nurses
         }
 
-        self.model.setObjective(self.transit_vars.prod(arch_weight) + (external_price * self.patient_vars.sum()))
-    
+        self.model.setObjective(
+            self.transit_vars.prod(arch_weight)
+            + (external_price * self.patient_vars.sum())
+        )
 
     def get_model(self):
         setattr(self.model, "_transit", self.transit_vars)
         setattr(self.model, "_nurses", self.nurses)
-        setattr(self.model, "_patient_count", len(self.nodes))        
+        setattr(self.model, "_patient_count", len(self.nodes))
         self.model.Params.lazyConstraints = 1
 
         return self.model, self.transit_vars
 
-    
+
 def subtour_elimination(model, where):
     if where == GRB.Callback.MIPSOL:
         vals = model.cbGetSolution(model._transit)
         for k in model._nurses:
-            selected = tuplelist((i, j) for _, i, j in model._transit.keys().select(k, '*', '*')
-                                 if vals[k,i,j] > 0.5)
+            selected = tuplelist(
+                (i, j)
+                for _, i, j in model._transit.keys().select(k, "*", "*")
+                if vals[k, i, j] > 0.5
+            )
             # find the shortest cycle in the selected edge list
             for tour in build_tours(selected):
-    
+
                 i, j = tour[0]
                 if i != 0:
-                    model.cbLazy(quicksum(model._transit[k, i, j] for i, j in tour) <= len(tour) - 1)
+                    model.cbLazy(
+                        quicksum(model._transit[k, i, j] for i, j in tour)
+                        <= len(tour) - 1
+                    )
--- nrs_lib/solver.py
+++ nrs_lib/solver.py
@@ -8,9 +8,11 @@ from .build_result import build_result
 
 def run_solver(config):
 
-    model_config = ModelConfigutator('Name')
+    model_config = ModelConfigutator("Name")
     model_config.set_variables(config["NURSES"], len(config["PATIENTS"]))
-    model_config.set_objective(config["HUB_DISTANCE"], config["PATIENTS_DISTANCE"], 10000)
+    model_config.set_objective(
+        config["HUB_DISTANCE"], config["PATIENTS_DISTANCE"], 10000
+    )
 
     model, transit = model_config.get_model()
 

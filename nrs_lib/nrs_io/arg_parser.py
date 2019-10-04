d6ac28d03a3e53d9516af7f90558ceb6aa1f4d39
Filippo <filipporanza@gmail.com>
2019-10-04 18:02:30 +0200

    add cli interface for Time and Gap stop

--- nrs_lib/nrs_io/arg_parser.py
+++ nrs_lib/nrs_io/arg_parser.py
@@ -21,4 +21,23 @@ def parse_args():
         "-d", "--debug", action="store_true", default=False, help="Enable debug mode"
     )
 
+    parser.add_argument(
+        "--gap",
+        help="""Specify the desidered gap between the current
+        solution and the LP relaxation. When this gap is reached the
+        execution is stopped
+        and the current best result is returned. If this option is 
+        used with --tmax the gap stop is enabled only after tmax has
+        elapse. By default gap stop is not enabled""",
+    )
+
+    parser.add_argument(
+        "--tmax",
+        help="""Specify maximum execution time, if the solver
+        does not find a solution in this time the execution is stopped
+        and the current best result is returned. If this option is 
+        used with --gap the gap stop is enabled only after tmax has
+        elapse. By default tmax is set to infinity""",
+    )
+
     return parser.parse_args()
